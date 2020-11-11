e95671800d1982db64e514a05aea45b3ddb0fbd6
Sergi Rourera <sergirourerallaurado@gmail.com>
2020-11-11 21:34:24 +0100

    First commit of home page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,36 @@ module.exports = {
   },
   purge: ['./src/*.html'],
   theme: {
-    extend: {},
+    extend: {
+      inset: {
+        '1/4': '25%',
+        '1/2': '50%',
+        '3/4': '75%'
+      },
+      zIndex: {
+        '-1': '-1',
+      },
+      maxWidth: {
+        'screen-h-10': '10vh',
+        'screen-h-20': '20vh',
+        'screen-h-30': '30vh',
+        'screen-h-40': '40vh',
+        'screen-h-50': '50vh',
+        'screen-h-60': '60vh',
+        'screen-h-70': '70vh',
+        'screen-h-80': '80vh',
+        'screen-h-90': '90vh',
+        'screen-w-10': '10vw',
+        'screen-w-20': '20vw',
+        'screen-w-30': '30vw',
+        'screen-w-40': '40vw',
+        'screen-w-50': '50vw',
+        'screen-w-60': '60vw',
+        'screen-w-70': '70vw',
+        'screen-w-80': '80vw',
+        'screen-w-90': '90vw'
+      }
+    },
   },
   variants: {},
   plugins: [],
