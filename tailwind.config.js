720676c23613f22c49eef2edd10e83602f164d94
Sergi Rourera <sergirourerallaurado@gmail.com>
2020-11-12 11:47:41 +0100

    Top section of home page.

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,12 +10,6 @@ module.exports = {
         'gray-dark': '#424242',
         'gray-light': '#b9b9b9'
       },
-      flex: {
-        '25': '0 0 25%',
-        '50': '0 0 50%',
-        '75': '0 0 75%',
-        '100': '0 0 100%',
-      },
       inset: {
         '1/4': '25%',
         '1/2': '50%',
@@ -24,35 +18,18 @@ module.exports = {
       zIndex: {
         '-1': '-1',
       },
-      maxWidth: {
-        'screen-h-10': '10vh',
-        'screen-h-20': '20vh',
-        'screen-h-30': '30vh',
-        'screen-h-40': '40vh',
-        'screen-h-50': '50vh',
-        'screen-h-60': '60vh',
-        'screen-h-70': '70vh',
-        'screen-h-80': '80vh',
-        'screen-h-90': '90vh',
-        'screen-w-10': '10vw',
-        'screen-w-20': '20vw',
-        'screen-w-30': '30vw',
-        'screen-w-40': '40vw',
-        'screen-w-50': '50vw',
-        'screen-w-60': '60vw',
-        'screen-w-70': '70vw',
-        'screen-w-80': '80vw',
-        'screen-w-90': '90vw'
+      maxHeight: {
+        '1280px': '1280px'
       },
       spacing: {
-        '10-px': '10px',
-        '15-px': '15px',
-        '20-px': '20px',
-        '25-px': '25px',
-        '50-px': '50px',
-        '75-px': '75px',
-        '90-px': '90px',
-        '115-px': '115px'
+        '10px': '10px',
+        '15px': '15px',
+        '20px': '20px',
+        '25px': '25px',
+        '50px': '50px',
+        '75px': '75px',
+        '90px': '90px',
+        '115px': '115px'
       }
     },
   },
