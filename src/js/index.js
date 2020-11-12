6ed39dfdc77eabe33e7b2d9874b443c16606ed21
Sergi Rourera <sergirourerallaurado@gmail.com>
2020-11-12 21:14:14 +0100

    New pages categories & libros mensuales

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,12 @@
 require('./header.js');
 
-const books = document.getElementById('libros-mensuales');
+const books = document.getElementById('libros-mensuales-grid');
 const moreBooks = document.getElementById('libros-mensuales-show-more');
 const allBooks = document.getElementById('libros-mensuales-show-all');
-if(moreBooks){
+if(moreBooks && books){
     moreBooks.addEventListener('click',()=>{
         books.classList.add('expanded');
         moreBooks.classList.add('hidden');
-        allBooks.classList.remove('float-right');
+        if(allBooks) allBooks.classList.remove('float-right');
     })
 }
